08abb9080dc3ed35e49513ada15bdcd3ba6b0a62
anas-1995 <anasalazmeh.95@gmail.com>
2018-09-27 19:40:24 +0300

    www. problem

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
+import { Router } from '@angular/router';
 import { ContactUsModalComponent } from './contact-us-modal/contact-us-modal.component';
 import { CallApiService } from './Services/call-api.service';
 import { GlobalService } from './Services/global.service';
 import { LoginService } from './Services/login.service';
 import { SignUpModalComponent } from './sign-up-modal/sign-up-modal.component';
 import { Component } from '@angular/core';
-import {MatDialog} from '@angular/material';
+import { MatDialog } from '@angular/material';
 
 @Component({
   selector: 'app-root',
@@ -12,22 +13,29 @@ import {MatDialog} from '@angular/material';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-    constructor(public logInSer: LoginService, public globalServ: GlobalService,  public APIServ: CallApiService, public dialog: MatDialog) {
-    
-  alert("sssss");
-  }
-  openContactUs(){
-       let dialogRef = this.dialog.open(ContactUsModalComponent, {
-            // width: '35%',
-        });
+  constructor(private router: Router, public logInSer: LoginService, public globalServ: GlobalService, public APIServ: CallApiService, public dialog: MatDialog) {
 
-        dialogRef.afterClosed().subscribe(result => {
-          if(result){
-            this.globalServ.errorDialog("إرسال رسالة","تمت عملية الإرسال بنجاح")
-          }
-        });
   }
-  
+
+  ngOnInit() {
+    let url = window.location.href;
+    if (url.indexOf("www.") != -1) {
+      url = url.replace("www.", '');
+      window.open(url, '_self');
+    }
+  }
+  openContactUs() {
+    let dialogRef = this.dialog.open(ContactUsModalComponent, {
+      // width: '35%',
+    });
+
+    dialogRef.afterClosed().subscribe(result => {
+      if (result) {
+        this.globalServ.errorDialog("إرسال رسالة", "تمت عملية الإرسال بنجاح")
+      }
+    });
+  }
+
 }
 
- 
+
