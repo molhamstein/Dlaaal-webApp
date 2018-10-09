57e4192842657a7863967206da8f8cf280fb33b3
anas-1995 <anasalazmeh.95@gmail.com>
2018-10-09 15:00:10 +0300

    change base url

--- dist/main.bundle.js
+++ dist/main.bundle.js
@@ -54,8 +54,8 @@ var CallApiService = /** @class */ (function () {
         //   "users",
         //   "users/login"
         // ];
-        // readonly baseUrl = "http://108.179.218.237:7500/api/"
-        this.baseUrl = "http://localhost:7500/api/";
+        this.baseUrl = "http://108.179.218.237:7500/api/";
+        // readonly baseUrl = "http://localhost:7500/api/"
         this.errorCode = 0;
         this.headers2 = this.headers2.append("Authorization", "Basic " + btoa("username:password"));
         // this.headers2 = this.headers2.append("Content-Type", "application/json");
